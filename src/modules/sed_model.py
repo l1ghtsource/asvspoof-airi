072b354b3e46d89fb713fda45f925161e369811c
lightsource <m2302070@edu.misis.ru>
2024-11-17 11:45:25 +0300

    add sed things

---
+++ src/modules/sed_model.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import partial
+from torchlibrosa.stft import Spectrogram, LogmelFilterBank
+from torchlibrosa.augmentation import SpecAugmentation
+from timm.models.efficientnet import tf_efficientnet_b0_ns
+
+from sed_modules import (
+    AttBlock,
+    init_layer,
+    init_bn,
+    do_mixup,
+    interpolate,
+    pad_framewise_output
+)
+
+encoder_params = {
+    'tf_efficientnet_b0_ns': {
+        'features': 1280,
+        'init_op': partial(tf_efficientnet_b0_ns, pretrained=True, drop_path_rate=0.2)
+    }
+}
+
+
+class AudioSEDModel(nn.Module):
+    def __init__(self, encoder, sample_rate, window_size, hop_size, mel_bins, fmin, fmax, classes_num):
+        super().__init__()
+
+        window = 'hann'
+        center = True
+        pad_mode = 'reflect'
+        ref = 1.0
+        amin = 1e-10
+        top_db = None
+        self.interpolate_ratio = 30  # downsampled ratio
+
+        # spectrogram extractor
+        self.spectrogram_extractor = Spectrogram(
+            n_fft=window_size,
+            hop_length=hop_size,
+            win_length=window_size,
+            window=window,
+            center=center,
+            pad_mode=pad_mode,
+            freeze_parameters=True
+        )
+
+        # logmel feature extractor
+        self.logmel_extractor = LogmelFilterBank(
+            sr=sample_rate,
+            n_fft=window_size,
+            n_mels=mel_bins,
+            fmin=fmin,
+            fmax=fmax,
+            ref=ref,
+            amin=amin,
+            top_db=top_db,
+            freeze_parameters=True
+        )
+
+        # Spec augmenter
+        self.spec_augmenter = SpecAugmentation(
+            time_drop_width=64,
+            time_stripes_num=2,
+            freq_drop_width=8,
+            freq_stripes_num=2
+        )
+
+        # model encoder
+        self.encoder = encoder_params[encoder]['init_op']()
+        self.fc1 = nn.Linear(encoder_params[encoder]['features'], 1024, bias=True)
+        self.att_block = AttBlock(1024, classes_num, activation='sigmoid')
+        self.bn0 = nn.BatchNorm2d(mel_bins)
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.fc1)
+        init_bn(self.bn0)
+
+    def forward(self, input, mixup_lambda=None):
+        '''Input : (batch_size, data_length'''
+
+        x = self.spectrogram_extractor(input)
+        # batch_size x 1 x time_steps x freq_bins
+        x = self.logmel_extractor(x)
+        # batch_size x 1 x time_steps x mel_bins
+
+        frames_num = x.shape[2]
+
+        x = x.transpose(1, 3)
+        x = self.bn0(x)
+        x = x.transpose(1, 3)
+        # print(x.shape)
+
+        if self.training:
+            x = self.spec_augmenter(x)
+
+        # mixup on spectrogram
+        if self.training and mixup_lambda is not None:
+            x = do_mixup(x, mixup_lambda)
+
+        # output shape (batch size, channels, time, frequency)
+        x = x.expand(x.shape[0], 3, x.shape[2], x.shape[3])
+        # print(x.shape)
+        x = self.encoder.forward_features(x)
+        # print(x.shape)
+        x = torch.mean(x, dim=3)
+        # print(x.shape)
+
+        x1 = F.max_pool1d(x, kernel_size=3, stride=1, padding=1)
+        x2 = F.avg_pool1d(x, kernel_size=3, stride=1, padding=1)
+        x = x1 + x2
+        # print(x.shape)
+
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = x.transpose(1, 2)
+        x = F.relu_(self.fc1(x))
+        x = x.transpose(1, 2)
+        x = F.dropout(x, p=0.5, training=self.training)
+        # print(x.shape)
+
+        (clipwise_output, norm_att, segmentwise_output) = self.att_block(x)
+        logit = torch.sum(norm_att * self.att_block.cla(x), dim=2)
+        segmentwise_output = segmentwise_output.transpose(1, 2)
+
+        # get framewise output
+        framewise_output = interpolate(segmentwise_output,
+                                       self.interpolate_ratio)
+        framewise_output = pad_framewise_output(framewise_output, frames_num)
+
+        output_dict = {
+            'framewise_output': framewise_output,
+            'logit': logit,
+            'clipwise_output': clipwise_output
+        }
+
+        return output_dict
---
+++ src/modules/sed_modules.py
@@ -0,0 +1,210 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+def init_layer(layer):
+    nn.init.xavier_uniform_(layer.weight)
+
+    if hasattr(layer, 'bias'):
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.)
+
+
+def init_bn(bn):
+    bn.bias.data.fill_(0.)
+    bn.weight.data.fill_(1.0)
+
+
+def init_weights(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        nn.init.xavier_uniform_(model.weight, gain=np.sqrt(2))
+        model.bias.data.fill_(0)
+    elif classname.find('BatchNorm') != -1:
+        model.weight.data.normal_(1.0, 0.02)
+        model.bias.data.fill_(0)
+    elif classname.find('GRU') != -1:
+        for weight in model.parameters():
+            if len(weight.size()) > 1:
+                nn.init.orghogonal_(weight.data)
+    elif classname.find('Linear') != -1:
+        model.weight.data.normal_(0, 0.01)
+        model.bias.data.zero_()
+
+
+def do_mixup(x: torch.Tensor, mixup_lambda: torch.Tensor):
+    '''Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes
+    (1, 3, 5, ...).
+    Args:
+      x: (batch_size * 2, ...)
+      mixup_lambda: (batch_size * 2,)
+    Returns:
+      out: (batch_size, ...)
+    """'''
+    out = (x[0::2].transpose(0, -1) * mixup_lambda[0::2] +
+           x[1::2].transpose(0, -1) * mixup_lambda[1::2]).transpose(0, -1)
+    return out
+
+
+class Mixup(object):
+    def __init__(self, mixup_alpha, random_seed=1234):
+        '''
+        Mixup coefficient generator.
+        '''
+        self.mixup_alpha = mixup_alpha
+        self.random_state = np.random.RandomState(random_seed)
+
+    def get_lambda(self, batch_size):
+        '''Get mixup random coefficients.
+        Args:
+          batch_size: int
+        Returns:
+          mixup_lambdas: (batch_size,)
+        '''
+        mixup_lambdas = []
+        for n in range(0, batch_size, 2):
+            lam = self.random_state.beta(self.mixup_alpha, self.mixup_alpha, 1)[0]
+            mixup_lambdas.append(lam)
+            mixup_lambdas.append(1. - lam)
+
+        return torch.from_numpy(np.array(mixup_lambdas, dtype=np.float32))
+
+
+def interpolate(x: torch.Tensor, ratio: int):
+    '''
+    Interpolate data in time domain. This is used to compensate the
+    resolution reduction in downsampling of a CNN.
+    Args:
+      x: (batch_size, time_steps, classes_num)
+      ratio: int, ratio to interpolate
+    Returns:
+      upsampled: (batch_size, time_steps * ratio, classes_num)
+    '''
+    (batch_size, time_steps, classes_num) = x.shape
+    upsampled = x[:, :, None, :].repeat(1, 1, ratio, 1)
+    upsampled = upsampled.reshape(batch_size, time_steps * ratio, classes_num)
+    return upsampled
+
+
+def pad_framewise_output(framewise_output: torch.Tensor, frames_num: int):
+    '''
+    Pad framewise_output to the same length as input frames. The pad value
+    is the same as the value of the last frame.
+    Args:
+      framewise_output: (batch_size, frames_num, classes_num)
+      frames_num: int, number of frames to pad
+    Outputs:
+      output: (batch_size, frames_num, classes_num)
+    '''
+    pad = framewise_output[:, -1:, :].repeat(
+        1, frames_num - framewise_output.shape[1], 1)
+    # tensor for padding
+
+    output = torch.cat((framewise_output, pad), dim=1)
+    # batch_size, frames_num, classes_num)
+
+    return output
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False
+        )
+
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+
+    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        x = F.relu_(self.bn2(self.conv2(x)))
+        if pool_type == 'max':
+            x = F.max_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg':
+            x = F.avg_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg+max':
+            x1 = F.avg_pool2d(x, kernel_size=pool_size)
+            x2 = F.max_pool2d(x, kernel_size=pool_size)
+            x = x1 + x2
+        else:
+            raise Exception('Incorrect argument!')
+
+        return x
+
+
+class AttBlock(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        activation="linear",
+        temperature=1.0
+    ):
+        super().__init__()
+
+        self.activation = activation
+        self.temperature = temperature
+        self.att = nn.Conv1d(
+            in_channels=in_features,
+            out_channels=out_features,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=True
+        )
+        self.cla = nn.Conv1d(
+            in_channels=in_features,
+            out_channels=out_features,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=True
+        )
+
+        self.bn_att = nn.BatchNorm1d(out_features)
+        self.init_weights()
+
+    def init_weights(self):
+        init_layer(self.att)
+        init_layer(self.cla)
+        init_bn(self.bn_att)
+
+    def forward(self, x):
+        # x: (n_samples, n_in, n_time)
+        norm_att = torch.softmax(torch.clamp(self.att(x), -10, 10), dim=-1)
+        cla = self.nonlinear_transform(self.cla(x))
+        x = torch.sum(norm_att * cla, dim=2)
+        return x, norm_att, cla
+
+    def nonlinear_transform(self, x):
+        if self.activation == 'linear':
+            return x
+        elif self.activation == 'sigmoid':
+            return torch.sigmoid(x)
